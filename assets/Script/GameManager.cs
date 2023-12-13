0baccbbc4274d1ffc61d70b2360bf1fa26eab8ea
fuujii256 <yoshihiro.fujita256@outlook.jp>
2023-12-14 06:31:57 +0900

    screen_fixed

--- assets/Script/GameManager.cs
+++ assets/Script/GameManager.cs
@@ -1,9 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class NewBehaviourScript : MonoBehaviour
+using UnityEngine.UI;
+public class GameManager : MonoBehaviour
 {
+    public GameObject Block_1;
+    public GameObject Block_2;
+    public GameObject Block_3;
+    public GameObject Block_4;
+    public GameObject Block_5;
+    public GameObject Block_6;
+    public GameObject Block_SP;
+
     // Start is called before the first frame update
     void Start()
     {
