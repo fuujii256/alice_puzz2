d7fcc6e6913957aac2b146355692706ea00b429d
fuujii256 <yoshihiro.fujita256@outlook.jp>
2023-12-14 07:59:18 +0900

    block1_move_first

--- assets/Script/Block_move.cs
+++ assets/Script/Block_move.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class Block_move : MonoBehaviour
 {
+    Rigidbody2D rbody;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Rigidbodyを取得
+        rbody = this.GetComponent<Rigidbody2D>();
+ 
+        //FreezePositionXをオンにする
+        rbody.constraints = RigidbodyConstraints2D.FreezePositionX; 
+        rbody.constraints = RigidbodyConstraints2D.FreezeRotation; 
     }
 
     // Update is called once per frame
--- assets/Script/GameManager.cs
+++ assets/Script/GameManager.cs
@@ -11,16 +11,37 @@ public class GameManager : MonoBehaviour
     public GameObject Block_5;
     public GameObject Block_6;
     public GameObject Block_SP;
+    
+    float game_Time = 0.0f;
+    float game_Time_Cnt = 0.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+    
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        GameObject block_1_Prefab = Resources.Load<GameObject>("Block_1");
+
+
+        game_Time_Cnt += Time.deltaTime;
+
+
+        if (game_Time_Cnt > 1.0f )
+        {
+
+            GameObject block_1 = Instantiate(block_1_Prefab); 
+
+
+            game_Time += game_Time_Cnt;
+            game_Time_Cnt = 0.0f;
+        }
+
         
+
     }
 }
