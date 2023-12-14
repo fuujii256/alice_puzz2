a14592bc0105758efaa4c887b527dc8475bdc00e
fuujii256 <yoshihiro.fujita256@outlook.jp>
2023-12-14 10:25:25 +0900

    block_move_second

--- assets/Script/Block_move.cs
+++ assets/Script/Block_move.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Block_move : MonoBehaviour
 {
     Rigidbody2D rbody;
+    bool player_control = true;  
+    int axisH_old = 0;
+    bool isFalling = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,11 +17,37 @@ public class Block_move : MonoBehaviour
         //FreezePositionXをオンにする
         rbody.constraints = RigidbodyConstraints2D.FreezePositionX; 
         rbody.constraints = RigidbodyConstraints2D.FreezeRotation; 
+
+          
+
     }
 
     // Update is called once per frame
     void Update()
-    {
-        
+    {        
+        if (player_control == true)
+        {
+            Vector3 pos = this.transform.position;
+            int axisH =(int)Input.GetAxisRaw("Horizontal");
+            if(axisH_old != axisH)
+            {
+             if (axisH == -1)
+                {
+                    pos.x -= 0.5f;
+                }
+                if(axisH == 1)
+                {
+                    pos.x += 0.5f;
+                }
+            }
+            this.transform.position = pos;
+            axisH_old = axisH;
+        }
+        //落下中か？
+        isFalling = rbody.velocity.y < 0.0f;  
+        if (isFalling ==false)
+        {
+            player_control = false;         //落下中以外は操作できない
+        }      
     }
 }
--- assets/Script/GameManager.cs
+++ assets/Script/GameManager.cs
@@ -11,7 +11,6 @@ public class GameManager : MonoBehaviour
     public GameObject Block_5;
     public GameObject Block_6;
     public GameObject Block_SP;
-    
     float game_Time = 0.0f;
     float game_Time_Cnt = 0.0f;
 
@@ -26,16 +25,41 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         GameObject block_1_Prefab = Resources.Load<GameObject>("Block_1");
-
+        GameObject block_2_Prefab = Resources.Load<GameObject>("Block_2");
+        GameObject block_3_Prefab = Resources.Load<GameObject>("Block_3");
+        GameObject block_4_Prefab = Resources.Load<GameObject>("Block_4");
+        GameObject block_5_Prefab = Resources.Load<GameObject>("Block_5");
 
         game_Time_Cnt += Time.deltaTime;
 
 
         if (game_Time_Cnt > 1.0f )
         {
-
-            GameObject block_1 = Instantiate(block_1_Prefab); 
-
+            //ブロックの新規生成
+            int rnd = Random.Range(1, 5);
+            float x = -3.76f;
+            float y = 3.24f;
+            float z = 0.0f;
+            Vector3 v3 = new Vector3(x,y,z);
+            switch(rnd)
+            {
+                case 1:
+                    Instantiate(block_1_Prefab,v3, Quaternion.Euler(0, 0, 0));     
+                    break;
+                case 2:
+                    Instantiate(block_2_Prefab,v3, Quaternion.Euler(0, 0, 0));           
+                    break;
+                case 3:
+                    Instantiate(block_3_Prefab,v3, Quaternion.Euler(0, 0, 0));  
+                    break;
+                case 4:
+                    Instantiate(block_4_Prefab,v3, Quaternion.Euler(0, 0, 0));  
+                    break; 
+                case 5:
+                    Instantiate(block_5_Prefab,v3, Quaternion.Euler(0, 0, 0));  
+                    break;
+            }                           
+            
 
             game_Time += game_Time_Cnt;
             game_Time_Cnt = 0.0f;
